231e39622442340ec05faf43d8aa886fd481e4ea
Luiz <luizfernandoribeirosobrinho84@gmail.com>
2026-03-10 08:18:17 -0300

    soma

---
+++ calculadora.py
@@ -0,0 +1,6 @@
+num1 = int(input("Digite seu primeiro numero:   "))
+num2 = int(input("Digite seu segundo numero:   "))
+operacao = input("Escolha a operacao desejada:  ")
+ 
+if operacao == "+":
+    print(num1 + num2)
